ee651322bb174ad6a6259f7df173860c9ed42f05
Pisgah <pisgah2002@gmail.com>
2024-06-21 21:18:01 -0400

    encryption and decryption

--- main.go
+++ main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello go-encrypt")
+	os.Remove("assets/ciphertext.bin")
+	os.Remove("assets/outputs/plaintext.txt")
+
+	encrypt()
+	decrypt()
+}
+
+func encrypt() {
+	plainText, err := os.ReadFile("assets/plaintext.txt")
+	if err != nil {
+		log.Fatal("Error reading file", err.Error())
+	}
+
+	key, err := os.ReadFile("assets/plaintext.txt")
+	if err != nil {
+		log.Fatal("Error reading file", err.Error())
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("cipher err: %v", err.Error())
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalf("cipher GCM err: %v", err.Error())
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		log.Fatalf("nonce err: %v", err.Error())
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	err = os.WriteFile("assets/ciphertext.bin", cipherText, 0777)
+	if err != nil {
+		log.Fatalf("write file err: %v", err.Error())
+	}
+}
+
+// Decrypt
+func decrypt() {
+	cipherText, err := os.ReadFile("assets/ciphertext.bin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := os.ReadFile("assets/key.txt")
+	if err != nil {
+		log.Fatalf("read file err: %v", err.Error())
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("cipher err: %v", err.Error())
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalf("cipher GCM err: %v", err.Error())
+	}
+
+	nonce := cipherText[:gcm.NonceSize()]
+	cipherText = cipherText[gcm.NonceSize():]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		log.Fatalf("decrypt file err: %v", err.Error())
+	}
+
+	err = os.WriteFile("assets/outputs/plaintext.txt", plainText, 0777)
+	if err != nil {
+		log.Fatalf("write file err: %v", err.Error())
+	}
 }
